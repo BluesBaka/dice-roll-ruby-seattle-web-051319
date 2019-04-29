567689cbc43bbc5a26d0d559c7cbea4f5a4d406c
Bryna Dunnells <badunnells@gmail.com>
2019-04-29 20:51:58 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,7 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-def roll
-  return rand(1...7)
+#def roll
+#  return rand(1...7)
 
+#end
+def roll
+  random = [1,2,3,4,5,6]
+  rand.random
 end
+
